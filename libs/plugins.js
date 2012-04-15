d15011cdbe24f32ee5d3b4ea843f4484c6601736
Axel Esquite <aesquite@gmail.com>
2012-04-15 11:08:45 -0400

    adding paul irish\'s universal log

---
+++ libs/plugins.js
@@ -0,0 +1,12 @@
+// usage: log('inside coolFunc', this, arguments);
+// paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
+log = function(){
+  log.history = log.history || [];   // store logs to an array for reference
+  log.history.push(arguments);
+  if(this.console) {
+      arguments.callee = arguments.callee.caller;
+      console.log( Array.prototype.slice.call(arguments) );
+  }
+};
+// make it safe to use console.log always
+(function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();)b[a]=b[a]||c})(console=console||{});
--- stickies.js
+++ stickies.js
@@ -4,27 +4,27 @@ if (Meteor.is_client) {
 	
 	Template.stickies.createNotes = function()
 	{
-		//console.log('stickies');
+		//log('stickies');
 		var notes = Notes.find({}, {sort:{created: -1, type:'notes'}}).fetch();
 		notes.unshift({type: 'insert', text: 'add your note here', author: {firstname: 'Axel',lastname: 'Esquite'}, created: new Date()})
-		//console.log('notes', notes);
+		//log('notes', notes);
 		return notes;
 	};
 	
 	Template.sticky.isNote = function(type)
 	{
-		//console.log('note', type, type === 'note');
+		//log('note', type, type === 'note');
 		return type === "note";
 	};
 	
 	Template.stickies.events = {
 		'click div.insert': function (e) {
-			var empty_note = {type: 'note', text: '...', author: {firstname: 'Some',lastname: 'Stranger'}, created: new Date()}
-			console.log('you pressed, add note to server', empty_note, arguments);
+			var empty_note = {type: 'note', text: '', author: {firstname: 'Some',lastname: 'Stranger'}, created: new Date()}
+			//log('you pressed, add note to server', empty_note, e);
 			Notes.insert(empty_note);
 		},
 		'keyup div.sticky form textarea': function(e) {
-			console.log('sticky form textarea', $(e.currentTarget).parent().attr('id').replace('sticky-', ''), $(e.currentTarget).val());
+			//log('sticky form textarea', $(e.currentTarget).parent().attr('id').replace('sticky-', ''), $(e.currentTarget).val());
 			Notes.update($(e.currentTarget).parent().attr('id').replace('sticky-', ''), {$set: {text: $(e.currentTarget).val()}});
 		}
 	};
